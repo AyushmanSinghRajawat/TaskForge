58fe00bbe641b6e1e3b2edda0cbb487b2cf3f3c4
Ayushman Singh Rajawat <ayushmanrajawatv@gmail.com>
2023-12-19 00:12:28 +0530

    mofified phase 1

--- public/index.js
+++ public/index.js
@@ -1,17 +1,83 @@
 // JavaScript to add a new box
 document.addEventListener("DOMContentLoaded", () => {
+    let u=0;
     function addNewDiv() {
         const container = document.getElementById("maindash");
         const newBox = document.createElement('div');
         const projectName = document.getElementById('inputProjectName').value;
         const projectDescription = document.getElementById('inputProjectDescription').value;
+        const projectkey = document.getElementById('inputProjectKey').value;
+        const projectmanager = document.getElementById('inputProjectManager').value;
+        const projectpriority = document.getElementById('priority').value;
+        const projecttags = document.getElementById('inputProjectTags').value;
+        const projectstartdate = document.getElementById('ProjectstartDate').value;
+        const projectenddate = document.getElementById('ProjectendDate').value;
+
 
         newBox.className = 'col-lg-4 mb-4';
+        u=u+1;
         newBox.innerHTML = `
         <div class="card">
-            <div class="card-body py-4">
+            <div class="card-body ps-4 pe-0 my-1 me-0">
             <h5 class="card-title"><a href="/task-dash" class="text-decoration-none">${projectName}</a></h5>
             <p class="card-text">${projectDescription}</p>
+            </div>
+            <hr class="py-0 my-0">
+            <button type="button" class="btn btn-light mx-0 my-1 px-0 py-0 btn-sm"  width="1em" height="1em" data-bs-toggle="modal" data-bs-target="#exampleModal${u}">
+            <i class="bi bi-info-circle"></i></button>
+        </div>
+        
+
+
+        
+        <div class="modal fade" id="exampleModal${u}" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        
+            <div class="modal-dialog modal-lg">
+                <div class="modal-content" style="background-color: black; color: white;">
+                    <div class="modal-header">
+                        <h1 class="modal-title fs-5" id="exampleModalLabel"><h2>Project Details</h2></h1>
+                        <button type="button" class="close btn btn-outline-danger px-2 py-0 rounded-0" data-bs-dismiss="modal" aria-label="Close"><span aria-hidden="true">X</span></button>
+                    </div>
+                    <div class="modal-body">
+                        <div class="project-info mx-2">
+                            <div class="row">
+                                <div class="col-md-6 ps-2 ">
+                                    <h5>Project Title:</h5>
+                                    <p>${projectName}</p>
+                                </div>
+                                <div class="col-md-6 ps-2 ">
+                                    <h5>Project Key:</h5>
+                                    <p>${projectkey}</p>
+                                </div>
+                                <div class="col-12 ps-2 ">
+                                    <h5>Project Description:</h5>
+                                    <p>${projectDescription}</p>
+                                </div>
+                                <div class="col-md-4 ps-2">
+                                    <h5>Project Manager:</h5>
+                                    <p>${projectmanager}</p>
+                                </div>
+                                <div class="col-md-3 ps-2">
+                                    <h5>Priority:</h5>
+                                    <p>${projectpriority}</p>
+                                </div>
+                                <div class="col-md-5 ps-2">
+                                    <h5>Tags or Categories:</h5>
+                                    <p>${projecttags}</p>
+                                </div>
+                                <div class="col-md-4 ps-2">
+                                    <h5>Start Date:</h5>
+                                    <p>${projectstartdate}</p>
+                                </div>
+                                <div class="col-md-4 ps-2">
+                                    <h5>End Date:</h5>
+                                    <p>${projectenddate}</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    
+                </div>
             </div>
         </div>
         `;
--- public/task.js
+++ public/task.js
@@ -1,6 +1,6 @@
 document.addEventListener("DOMContentLoaded", () => {
 
-
+    let a=0;
     function addNewCol() {
         console.log("buttonclicked")
         const task_dash = document.getElementById("task-main");
@@ -12,8 +12,6 @@ document.addEventListener("DOMContentLoaded", () => {
         // Split the input date into parts using "-" as the separator
         let dateParts = TaskDate.split("-");
 
-        // // Rearrange the parts to form the desired format "28/09/23"
-        // let formattedDate = `${dateParts[2].substring(2)}/${dateParts[1]}/${dateParts[0].substring(2)}`;
         let formattedDate = `${dateParts[2].substring(0)}/${dateParts[1]}/${dateParts[0].substring(2)}`;
 
         console.log(formattedDate); // Output: "28/09/23"
@@ -23,7 +21,10 @@ document.addEventListener("DOMContentLoaded", () => {
 
         const taskDescription = document.getElementById('inputTaskDescription').value;
         // const TaskStatus=document.getElementById('inputStatus').value;
-
+        const taskTitle=document.getElementById('inputTaskTitle').value;
+        const taskid=document.getElementById('inputTaskId').value;
+        const taskTag=document.getElementById('inputTaskTags').value;
+        const taskstart=document.getElementById('TaskstartDate').value;
         // Get the value of TaskStatus
         const TaskStatus = document.getElementById('inputStatus').value;
 
@@ -39,12 +40,12 @@ document.addEventListener("DOMContentLoaded", () => {
         task_status = '<span class="mb-0 badge bg-success-subtle border border-4 border-success-subtle text-success-emphasis rounded-pill">Completed</span>';
         }
 
-// Now, task_status will contain the appropriate HTML based on the selected value of TaskStatus
 
 
         const Assignedto=document.getElementById('inputAssignedTo').value;
         console.log("newdivcreated")
         newBoxTask.className = 'col-lg-12 card mb-1 py-1 px-3 rounded-2 t';
+        a=a+1;
         
         newBoxTask.innerHTML = `
         <div class="row g-3">
@@ -52,7 +53,9 @@ document.addEventListener("DOMContentLoaded", () => {
           ${task_status}
           </div>
           <div class="col-lg-8 px-3">
-            <span>${taskDescription}</span>
+            <span><button type="button" class="btn btn-light py-0 my-0 " data-bs-toggle="modal" data-bs-target="#exampleModal${a}">${taskDescription}
+          </button>
+</span>
           </div>
           <div class="col-lg-1 border border-1 rounded-1 border-dark px-0 ps-1">${formattedDate}</div>
           <div class="col-lg-2">
@@ -61,6 +64,61 @@ document.addEventListener("DOMContentLoaded", () => {
             </span>
           </div>
         </div>
+        <div class="modal fade" id="exampleModal${a}" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                <div class="modal-dialog modal-lg">
+                  <div class="modal-content" style="background-color: black; color: white;">
+                    <div class="modal-header">
+                      <h1 class="modal-title fs-5" id="exampleModalLabel"><h2>Task Details</h2></h1>
+                      <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                    </div>
+                    <div class="modal-body">
+                          <div class="task-info">
+                          
+                          <div class="row">
+                              <div class="col-md-6">
+                                  <h5>Task Title:</h5>
+                                  <p>${taskTitle}</p>
+                              </div>
+                              <div class="col-md-6">
+                                  <h5>Task Id:</h5>
+                                  <p>${taskid}</p>
+                              </div>
+                              <div class="col-12">
+                                  <h5>Task Description:</h5>
+                                  <p>${taskDescription}</p>
+                              </div>
+                              <div class="col-md-4">
+                                  <h5>Assigned to:</h5>
+                                  <p>${Assignedto}</p>
+                              </div>
+                              <div class="col-md-3">
+                                  <h5>Status:</h5>
+                                  <p>${task_status}</p>
+                              </div>
+                              <div class="col-md-5">
+                                  <h5>Tags or Categories:</h5>
+                                  <p>${taskTag}</p>
+                              </div>
+                              <div class="col-md-4">
+                                  <h5>Start Date:</h5>
+                                  <p>${taskstart}</p>
+                              </div>
+                              <div class="col-md-4">
+                                  <h5>End Date:</h5>
+                                  <p>${TaskDate}</p>
+                              </div>
+
+                          </div>
+                      </div>
+                    </div>
+                    <div class="modal-footer">
+                      <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                      <button type="button" class="btn btn-primary">Save changes</button>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              
         `;
         console.log("innerhtmladded");
         task_dash.insertAdjacentElement('beforeend', newBoxTask);
